2a65d07a0bef5282244c52566659e7638251a5c3
Ishmael K <kaayishy@gmail.com>
2026-09-08 00:30:10 +0200

    uml features implemented - state + makefile & commented execute for now

--- ProductionUnit.h
+++ ProductionUnit.h
@@ -10,7 +10,7 @@ class ProductionUnit {
         virtual std::string getName() const  = 0;
         virtual std::string getStatus() const = 0;
         virtual bool isUrgent() const = 0;
-        virtual void execute() = 0; // This method is necessary for Decorator (distinguishes different behaviours of Decorators)
+        // virtual void execute() = 0; // This method is necessary for Decorator (distinguishes different behaviours of Decorators)
         virtual ~ProductionUnit() {} 
 
 };
