a2905a5203b459b3a4fecac9e64e54c70dbf373b
topboy-taku <takunda.mugwagwa@icloud.com>
2026-09-08 02:41:33 +0200

    Updated Shot.h fixed errors

--- Shot.h
+++ Shot.h
@@ -1,31 +1,25 @@
 #ifndef SHOT_H
 #define SHOT_H
-
 #include "ProductionUnit.h"
-
 class ShotState;
-
 class Shot : public ProductionUnit {
     public:
         Shot(const std::string& name);
         std::string getName() const override;
         std::string getStatus() const override;
         bool isUrgent() const override;
-
+        void execute() override;
          // Lifecycle transitions, delegated to the current ShotState
         void advance();
         void reject();
-
         //Called by ShotState subclasses to change this shot's current state.
         // Shot owns its state and is responsible for deleting the old one.
         void setState(ShotState* newState);
         
         virtual ~Shot();
-
         
     private:
         std::string name;
         ShotState* state;
 };
-
 #endif
